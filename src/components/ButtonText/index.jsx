f6b896c7154784588a3b983a5d9520797b45de7b
José Cássio <josecassio2013@gmail.com>
2023-05-10 15:24:15 -0300

    update buttontext

--- src/components/ButtonText/index.jsx
+++ src/components/ButtonText/index.jsx
@@ -2,11 +2,12 @@ import { useNavigate } from "react-router-dom"
 
 import * as C from "./styles"
 
-export function ButtonText({text, link, ...rest}){
+export function ButtonText({text, link, icon: Icon, ...rest}){
     const navigate = useNavigate();
 
     return(
     <C.Container {...rest} onClick={() => navigate(link)}>
+       {Icon && <Icon/>}
        {text}
     </C.Container>
     )
