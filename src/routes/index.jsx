086bc99f18cdd80742d5b5050ad3b5ce3c943598
José Cássio <josecassio2013@gmail.com>
2023-05-27 18:30:49 -0300

    fix isAdmin bug

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -9,7 +9,7 @@ import { useAuth } from "../hooks/auth";
 
 export function Routes(){
     const { user } = useAuth();
-    const isAdmin = user.isAdmin === 1;
+    const isAdmin = user && user.isAdmin === 1;
     return(
         <BrowserRouter>
             {user ? (isAdmin ? <AdminRoutes/>: <AppRoutes/>) : <AuthRoutes/> }
